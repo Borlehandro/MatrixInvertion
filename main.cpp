858a536c9740dee7691d10fa04b80478a78fc253
Alex Borzikov <a.borzikov@g.nsu.ru>
2019-12-05 20:39:48 +0700

    Invention for test matrix is correct. Also checking is added. Need a lot of refactor.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+/// OK!
 float generateMatrixAndCountMinor (float* matrix, int n) {
     std::srand(time(NULL));
     float maxMinor = 0;
@@ -17,6 +18,7 @@ float generateMatrixAndCountMinor (float* matrix, int n) {
     return maxMinor;
 }
 
+/// OK!
 void generateOneMatrix(float* matrix, int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++)
@@ -24,6 +26,7 @@ void generateOneMatrix(float* matrix, int n) {
     }
 }
 
+/// OK!
 float transposeAndCountMinor(const float* matrix, float *transp, int n) {
     float maxMinor = 0;
     for (int j=0; j<n; j++) {
@@ -38,10 +41,11 @@ float transposeAndCountMinor(const float* matrix, float *transp, int n) {
     return maxMinor;
 }
 
-void divideToScalar(const float* matrix, float* result, int n, float scalar){
+/// OK!
+void divideToScalarAndCountResultTranspose(const float* matrix, float* result, float* resultTrans, int n, float scalar){
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++)
-            result[i*n+j] = matrix[i*n+j]/scalar;
+            resultTrans[j*n+i] = result[i * n + j] = matrix[i * n + j] / scalar;
     }
 }
 
@@ -56,6 +60,9 @@ void multiplyToTransposed(const float* matrix, const float* transposed, float* r
         for (int j=0; j<n; j++) {
             float currentSum = 0;
             for(int k=0; k<n; k++){
+                //For debug
+                auto a = matrix[j*n+k];
+                auto b = transposed[i*n+k];
                 currentSum+= matrix[j*n+k]*transposed[i*n+k];
             }
             result[j*n+i] = currentSum;
@@ -63,11 +70,91 @@ void multiplyToTransposed(const float* matrix, const float* transposed, float* r
     }
 }
 
+/// OK!
+void countR(const float *one, const float *b, const float *transposed, float *r, int n){
+    float t[n*n];
+
+    cout << "\n b inside \n";
+    for(int i=0; i<n; i++) {
+        for (int j = 0; j < n; j++)
+            cout << b[i * n + j] << " ";
+        cout << "\n";
+    }
+    cout << "\n";
+
+    multiplyToTransposed(b, transposed, t, n);
+    cout << "\n PROM \n";
+    for(int i=0; i<n; i++) {
+        for (int j = 0; j < n; j++)
+            cout << t[i * n + j] << " ";
+        cout << "\n";
+    }
+    cout << "\n";
+
+    for(int i=0; i<n; i++)
+        for(int j=0; j<n; j++)
+            r[i*n+j] = one[i*n+j] - t[i*n+j];
+}
+
+void countSeries(const float *one, const float *r, float *result, int n, int m) {
+    //Todo Refactor
+    if(m == 1) {
+        for(int i=0; i<n; i++)
+            for(int j=0; j<n; j++)
+                result[i*n+j] = one[i*n+j];
+        return;
+    } else if(m == 2) {
+        for(int i=0; i<n; i++)
+            for(int j=0; j<n; j++)
+                result[i*n+j] = one[i*n+j] + r[i*n+j];
+        return;
+    } else {
+        float current[n*n];
+        for(int i=0; i<n; i++)
+            for(int j=0; j<n; j++) {
+                result[i * n + j] = one[i * n + j] + r[i * n + j];
+                current[j * n + i] = r[i * n + j];
+            }
+
+
+        for (int k=0; k<m-2; k++) {
+            float multed[n*n];
+            multiplyToTransposed(r, current, multed, n);
+
+
+
+            cout << "\n MULTED \n";
+            for(int i=0; i<n; i++) {
+                for (int j = 0; j < n; j++)
+                    cout << multed[i*n + j] << " ";
+                cout << "\n";
+            }
+            cout << "\n";
+
+            for(int i=0; i<n; i++)
+                for(int j=0; j<n; j++) {
+                    result[i * n + j] += multed[i * n + j];
+                    current[j * n + i] = multed[i * n + j];
+                }
+        }
+
+    }
+    cout << "\n  inside res \n";
+    for(int i=0; i<n; i++) {
+        for (int j = 0; j < n; j++)
+            cout << result[i*n + j] << " ";
+        cout << "\n";
+    }
+    cout << "\n";
+}
+
 int main() {
     int n,m;
     cin >> n >> m;
-    float matrix[n*n], one[n*n], transp[n*n], b[n*n], test[n*n];
-    float minor = generateMatrixAndCountMinor(matrix, n);
+    float one[n*n], transp[n*n], b[n*n], bTrans[n*n], r[n*n];
+    // float minor = generateMatrixAndCountMinor(matrix, n);
+    float matrix[] = {5,8,6,4,3,2,0,1,4};
+    float minor = 19;
     cout << "minor  is " << minor << "\n";
     for (int i=0; i<n; i++) {
         for (int j = 0; j < n; j++)
@@ -76,28 +163,6 @@ int main() {
     }
     // Matrix generation and minor ||A||1 counting is correct
     generateOneMatrix(one, n);
-    cout << "\n";
-    for (int i=0; i<n; i++) {
-        for (int j = 0; j < n; j++)
-            cout << one[i*n+j] << " ";
-        cout << "\n";
-    }
-    cout << "\n";
-    float minor2 = transposeAndCountMinor(matrix, transp, n);
-    cout << "minor is " << minor2 << "\n";
-    for (int i=0; i<n; i++) {
-        for (int j = 0; j < n; j++)
-            cout << transp[i*n+j] << " ";
-        cout << "\n";
-    }
-    cout << "\n";
-    divideToScalar(matrix, b, n, minor*minor2);
-    for (int i=0; i<n; i++) {
-        for (int j = 0; j < n; j++)
-            cout << b[i*n+j] << " ";
-        cout << "\n";
-    }
-    cout << "\n";
 
     //For matrix multiplication test
 
@@ -111,7 +176,63 @@ int main() {
         cout << "\n";
     }*/
 
+    float q[3*3];
 
+    float minor2 = transposeAndCountMinor(matrix, q, n);
+    cout << "minor is !!!" << minor2 << "\n";
+    for (int i=0; i<n; i++) {
+        for (int j = 0; j < n; j++)
+            cout << q[i*n+j] << " ";
+        cout << "\n";
+    }
+    cout << "\n";
+
+    cout << "B:\n";
+    divideToScalarAndCountResultTranspose(q, b, bTrans, n, minor * minor2);
+    for (int i=0; i<n; i++) {
+        for (int j = 0; j < n; j++)
+            cout << b[i*n+j] << " ";
+        cout << "\n";
+    }
+    cout << "\n B_trans \n";
+    for (int i=0; i<n; i++) {
+        for (int j = 0; j < n; j++)
+            cout << bTrans[i*n+j] << " ";
+        cout << "\n";
+    }
+    cout <<"\n R: \n";
+
+    countR(one, b, q, r, n);
+    for (int i=0; i<n; i++) {
+        for (int j = 0; j < n; j++)
+            cout << r[i * n + j] << " ";
+        cout << "\n";
+    }
+
+    float series[n*n];
+    countSeries(one, r, series, n, m);
+
+    float result[n*n];
+    multiplyToTransposed(series, bTrans, result, n);
+    cout << "\n RES: \n";
+    for (int i=0; i<n; i++) {
+        for (int j = 0; j < n; j++)
+            cout << result[i * n + j] << " ";
+        cout << "\n";
+    }
+
+    // Checking
+    float rt[n*n];
+    transposeAndCountMinor(result, rt, n);
+    float check[n*n];
+    multiplyToTransposed(matrix, rt, check, n);
+
+    cout << "\n CHECK: \n";
+    for (int i=0; i<n; i++) {
+        for (int j = 0; j < n; j++)
+            cout << check[i * n + j] << " ";
+        cout << "\n";
+    }
 
     return 0;
 }
